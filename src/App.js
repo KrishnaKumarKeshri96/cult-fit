eef81a2e58964c81f75fb3b173f083c807d03575
krishna <keshri819@gmail.com>
2021-09-29 23:51:56 +0530

    Cross Button working

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div>
       <Login />
+
       <CartPage />
     </div>
   );
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,41 +1,63 @@
 import styles from "./login.module.css";
 import { FaTimes, FaGoogle, FaFacebookF, FaEnvelope } from "react-icons/fa";
+import { useState } from "react";
+import styled from "styled-components";
 
 export const Login = () => {
+  const [hide, setHide] = useState(true);
+
+  const handleHide = () => {
+    setHide(true);
+  };
+
+  const LoginDiv = styled.div`
+    visibility: ${hide ? "hidden" : "visible"};
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 2000;
+    background-color: rgba(128, 128, 128, 0.555);
+  `;
+
   return (
-    <div className={styles["loginContainer"]}>
-      <div className={styles["login"]} id="login">
-        <div className={styles["login__cross"]}>
-          <button>
-            <FaTimes />
+    <>
+      <LoginDiv>
+        <div className={styles["login"]}>
+          <div className={styles["login__cross"]}>
+            <button onClick={handleHide}>
+              <FaTimes />
+            </button>
+          </div>
+          <img
+            src="https://static.cure.fit/assets/images/curefit_login_logo.svg"
+            alt="logo"
+          />
+          <img
+            src="https://static.cure.fit/assets/images/curefit-name.svg"
+            alt="name"
+          />
+
+          <input type="phone" placeholder="Enter your phone number" />
+          <button className={styles["login__continueBtn"]}>Continue</button>
+          <li>
+            By Continuing you agree to the
+            <em style={{ color: "red" }}>Terms of Services</em> and
+            <em style={{ color: "red" }}>Privacy policy</em>.
+          </li>
+
+          <button className={styles["login__google"]}>
+            Continue with
+            <span>
+              <FaGoogle />
+              <FaFacebookF />
+              <FaEnvelope />
+            </span>
           </button>
         </div>
-        <img
-          src="https://static.cure.fit/assets/images/curefit_login_logo.svg"
-          alt="logo"
-        />
-        <img
-          src="https://static.cure.fit/assets/images/curefit-name.svg"
-          alt="name"
-        />
-
-        <input type="phone" placeholder="Enter your phone number" />
-        <button className={styles["login__continueBtn"]}>Continue</button>
-        <li>
-          By Continuing you agree to the
-          <em style={{ color: "red" }}>Terms of Services</em> and
-          <em style={{ color: "red" }}>Privacy policy</em>.
-        </li>
-
-        <button className={styles["login__google"]}>
-          Continue with
-          <span>
-            <FaGoogle />
-            <FaFacebookF />
-            <FaEnvelope />
-          </span>
-        </button>
-      </div>
-    </div>
+      </LoginDiv>
+      <button onClick={() => setHide(false)}>click Me</button>
+    </>
   );
 };
