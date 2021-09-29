016fa68e1c867f23b597ed984f18522bcf7f27ee
HardikThummar97 <hardikthummar97@gmail.com>
2021-09-29 13:12:49 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Store from "./components/store/Store";
 
 function App() {
   //testing
@@ -7,20 +7,7 @@ function App() {
   //test slack notificatin
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Store />
     </div>
   );
 }
