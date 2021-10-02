1c7c43b3eb60ce27d43c3b52c462612e99a892c9
“Sagar <mrsagar105@gmail.com>
2021-10-02 23:39:24 +0530

    Updated landing page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 
 import { LandingPage } from "./Components/Landing_Page_Sec/LandingPage/LandingPage";
-import Store from "./Components/store/Store";
-import { ProductPage } from "./Components/store/StoreProductPage";
-import { CartPage } from "./Components/Cart/CartPage";
+// import Store from "./Components/store/Store";
+// import { ProductPage } from "./Components/store/StoreProductPage";
+// import { CartPage } from "./Components/Cart/CartPage";
 
 function App() {
   return (
@@ -12,15 +12,6 @@ function App() {
       <Switch>
         <Route path="/" exact>
           <LandingPage />
-        </Route>
-        <Route path="/cultstore">
-          <Store />
-        </Route>
-        <Route path="/landing/:id">
-          <CartPage></CartPage>
-        </Route>
-        <Route path="/productpage/:id">
-          <ProductPage></ProductPage>
         </Route>
         <Route>Page Not Found</Route>
       </Switch>
