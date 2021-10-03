c65464f3ae6e512b5af4dd572f45e8a1d31b273f
krishna <keshri819@gmail.com>
2021-10-03 21:39:53 +0530

    Deleted src

--- src/Contexts/AppContext.jsx
+++
@@ -1,8 +0,0 @@
-const { createContext } = require("react");
-
-export const AppContext = createContext();
-
-export const AppContextProvider = ({ children }) => {
-    
-  return <AppContext.Provider>{children}</AppContext.Provider>;
-};
